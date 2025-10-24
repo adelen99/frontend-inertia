da8fafe94083f2ae8e7b9fc2559709f37b38d257
adelen99 <lupuadelin1999@yahoo.com>
2025-10-24 12:10:00 +0300

    feat: display validation errors in UserForm component

--- src/components/users/UserForm.tsx
+++ src/components/users/UserForm.tsx
@@ -1,6 +1,5 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
 import { userFormSchema, UserFormValues } from "@/lib/validations/users";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
@@ -11,6 +10,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
+import { cn } from "@/lib/utils";
 
 interface UserForm {
   onSubmit: (data: UserFormValues) => void;
@@ -31,7 +31,12 @@ export const UserForm = ({
   isLoading = false,
   formId = "user-form",
 }: UserForm) => {
-  const { register, handleSubmit, control } = useForm<UserFormValues>({
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<UserFormValues>({
     resolver: zodResolver(userFormSchema),
     defaultValues,
   });
@@ -41,38 +46,58 @@ export const UserForm = ({
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="flex flex-col gap-6">
           <div className="grid gap-2">
-            <Label htmlFor="name">First Name*</Label>
+            <Label htmlFor="first_name">First Name*</Label>
             <Input
               id="first_name"
               type="text"
               {...register("first_name")}
               disabled={isLoading}
+              className={cn(errors.first_name && "border-red-500")}
+              aria-invalid={!!errors.first_name}
             />
+            {errors.first_name && (
+              <span className="text-sm text-red-500">
+                {errors.first_name.message}
+              </span>
+            )}
           </div>
 
           <div className="grid gap-2">
             <Label htmlFor="last_name">Last Name*</Label>
             <Input
               id="last_name"
-              type="last_name"
+              type="text"
               {...register("last_name")}
               disabled={isLoading}
+              className={cn(errors.last_name && "border-red-500")}
+              aria-invalid={!!errors.last_name}
             />
+            {errors.last_name && (
+              <span className="text-sm text-red-500">
+                {errors.last_name.message}
+              </span>
+            )}
           </div>
 
           <div className="grid gap-2">
             <Label htmlFor="email">Email*</Label>
             <Input
               id="email"
-              type="tel"
+              type="email" //
               {...register("email")}
               disabled={isLoading}
+              className={cn(errors.email && "border-red-500")}
+              aria-invalid={!!errors.email}
             />
+            {errors.email && (
+              <span className="text-sm text-red-500">
+                {errors.email.message}
+              </span>
+            )}
           </div>
         </div>
 
         <div className="flex flex-col gap-6">
-          {" "}
           <div className="grid gap-2">
             <Label htmlFor="password">Password*</Label>
             <Input
@@ -80,27 +105,47 @@ export const UserForm = ({
               type="password"
               {...register("password")}
               disabled={isLoading}
+              className={cn(errors.password && "border-red-500")}
+              aria-invalid={!!errors.password}
             />
+            {errors.password && (
+              <span className="text-sm text-red-500">
+                {errors.password.message}
+              </span>
+            )}
           </div>
+
           <div className="grid gap-2">
             <Label htmlFor="role">Role*</Label>
             <Controller
               name="role"
               control={control}
-              render={({ field }) => (
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  disabled={isLoading}
-                >
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Select a role" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="user">User</SelectItem>
-                    <SelectItem value="owner">Owner</SelectItem>
-                  </SelectContent>
-                </Select>
+              render={({ field, fieldState }) => (
+                <>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    disabled={isLoading}
+                  >
+                    <SelectTrigger
+                      className={cn(
+                        "w-full",
+                        fieldState.error && "border-red-500"
+                      )}
+                    >
+                      <SelectValue placeholder="Select a role" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="user">User</SelectItem>
+                      <SelectItem value="owner">Owner</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  {fieldState.error && (
+                    <span className="text-sm text-red-500">
+                      {fieldState.error.message}
+                    </span>
+                  )}
+                </>
               )}
             />
           </div>
