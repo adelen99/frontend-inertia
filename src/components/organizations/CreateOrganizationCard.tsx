3ebce80eb4a80228970972af06ade2b9932143ac
adelen99 <lupuadelin1999@yahoo.com>
2025-10-24 09:58:31 +0300

    fix: misplaced OrganizationForm

--- src/components/organizations/CreateOrganizationCard.tsx
+++ src/components/organizations/CreateOrganizationCard.tsx
@@ -10,22 +10,23 @@ import {
   CardTitle,
 } from "../ui/card";
 import { Button } from "../ui/button";
-import { ContactFormValues } from "@/lib/validations/contacts";
-import { useCreateContact } from "@/lib/api/contacts";
-import { ContactForm } from "../contacts/ContactForm";
+
+import { OrganizationForm } from "./OrganizationForm";
+import { OrganizationFormData } from "@/lib/validations/organization";
+import { useCreateOrganization } from "@/lib/api/organizations";
 
 const CreateOrganizationCard = () => {
   const router = useRouter();
-  const createMutation = useCreateContact();
+  const createMutation = useCreateOrganization();
 
-  const handleCreate = (data: ContactFormValues) => {
+  const handleCreate = (data: OrganizationFormData) => {
     createMutation.mutate(data, {
       onSuccess: () => {
-        toast.success("Contact created successfully!");
-        router.push("/contacts");
+        toast.success("Organization created successfully!");
+        router.push("/organizations");
       },
       onError: () => {
-        toast.error("Failed to create contact");
+        toast.error("Failed to create organization");
       },
     });
   };
@@ -35,24 +36,24 @@ const CreateOrganizationCard = () => {
       <CardHeader>
         <CardTitle>
           <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-            Create New Contact
+            Create New Organization
           </h2>
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <ContactForm
+        <OrganizationForm
           onSubmit={handleCreate}
           isLoading={createMutation.isPending}
-          formId="create-contact-form"
+          formId="create-org-form"
         />
       </CardContent>
       <CardFooter className="flex justify-end">
         <Button
           type="submit"
-          form="create-contact-form"
+          form="create-org-form"
           disabled={createMutation.isPending}
         >
-          Create contact
+          Create organization
         </Button>
       </CardFooter>
     </Card>
